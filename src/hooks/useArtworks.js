63faffe7187aaaccc9cc42f9922a1bd19f37f22e
Akshat <masterakshata@gmail.com>
2021-05-02 02:16:11 +0530

    fix refetching data

--- src/hooks/useArtworks.js
+++ src/hooks/useArtworks.js
@@ -1,20 +1,26 @@
 import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { ADD_DATA } from '../actions';
 import fetchArt from '../api/fetchArt';
 
 const useArtWorks = () => {
   const [loading, setLoading] = useState(true);
-
+  const data = useSelector((state) => state.art.artworks);
+  let shouldFetchData = true;
+  if (data.length !== 0) {
+    shouldFetchData = false;
+  }
   const dispatch = useDispatch();
   useEffect(async () => {
-    const data1 = fetchArt();
-    const data2 = fetchArt(8);
-    const data3 = fetchArt(20);
+    if (shouldFetchData) {
+      const data1 = fetchArt();
+      const data2 = fetchArt(8);
+      const data3 = fetchArt(20);
 
-    dispatch(ADD_DATA(await data1));
-    dispatch(ADD_DATA(await data2));
-    dispatch(ADD_DATA(await data3));
+      dispatch(ADD_DATA(await data1));
+      dispatch(ADD_DATA(await data2));
+      dispatch(ADD_DATA(await data3));
+    }
     setLoading(false);
   }, []);
 

08399947e9e78a485865b85bd47d9b3d4a262f63
Akshat <masterakshata@gmail.com>
2021-05-02 18:57:46 +0530

    enable redux dev extension

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,6 +3,7 @@ import artReducer from './art';
 
 const store = createStore(combineReducers({
   art: artReducer,
-}));
+// eslint-disable-next-line no-underscore-dangle
+}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
